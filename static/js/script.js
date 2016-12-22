1e9fa7c35ca0543af487018f13c76135f41e5685
Marian Petruk <marian.petruk@gmail.com>
2016-12-22 23:42:01 +0200

    upd

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
-(function() {
-
-}).call(this);
+window.onload=function(){
+	console.log("Start");
+	info_button = document.getElementById("info_button");
+	info = document.getElementById("info_table");
+	info_button.onclick = function(){
+		console.log("display = " + info.style.display);
+		console.log(info.style.display == 'none');
+		if(info.style.display == 'none'){
+			info.style.display = 'block';
+		}
+		else if(info.style.display == 'block'){
+			info.style.display = 'none';
+		}
+		if(info.style.display == ''){
+			info.style.display = 'block';
+		}
+	}
+}
