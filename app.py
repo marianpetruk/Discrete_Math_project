748542619a31d71624ec1b26cb387d5d72539a80
Marian Petruk <marian.petruk@gmail.com>
2016-12-22 19:17:28 +0200

    upd

--- app.py
+++ app.py
@@ -13,6 +13,25 @@ def about():
     return render_template('pages/placeholder.about.html')
 
 
+@app.route('/truth_table', methods=['GET', 'POST'])
+def truth():
+    if request.method == 'GET':
+        return render_template('pages/placeholder.truth_table.html')
+    
+
+
+@app.route('/worshell', methods=['GET', 'POST'])
+def worshell():
+    if request.method == 'GET':
+        return render_template('pages/placeholder.worshell.html')
+
+
+@app.route('/relations', methods=['GET', 'POST'])
+def relations():
+    if request.method == 'GET':
+        return render_template('pages/placeholder.relations.html')
+
+
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('errors/500.html'), 500
