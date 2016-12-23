39917f43a7ac6181a3a64725c70e63d3d88c8051
vzabulskyy <v.zabulskyy@gmail.com>
2016-12-23 12:19:08 +0200

    test2

--- static/python/relations.py
+++ static/python/relations.py
@@ -142,5 +142,3 @@ def main(matrix_):
                                                return_text(anti_symmetric_relation(matrix_)),
                                                return_text(transitive_relation(matrix_)))
     return text.split('\n')
-
-i = 0
