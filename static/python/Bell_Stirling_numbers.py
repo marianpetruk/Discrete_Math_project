20a2cea664a980f487795e1fb2cc15236bc2c597
Roman Vey <roman.vey@gmail.com>
2017-01-30 00:05:01 +0200

    Bell/Stirling numbers added Localization Some bugs fixed

---
+++ static/python/Bell_Stirling_numbers.py
@@ -0,0 +1,52 @@
+import re
+
+
+def B(n):
+    if n > 20:
+        return "BIG"
+    sum = 0
+    try:
+        for i in range(n + 1):
+            sum += S(n, i)
+        return sum
+    except:
+        return "INVALID"
+
+
+def S(n, k):
+    if k > n:
+        return "ERR"
+    if n > 20:
+        return "BIG"
+    if k == n:
+        return 1
+    if n > 0 and k == 0:
+        return 0
+    return S(n - 1, k - 1) + k * S(n - 1, k)
+
+
+def main(s, lg):
+    print(B(3))
+    bell = re.compile('B\(\d+\)')
+    stirling = re.compile('S\(\d+,\s*\d+\)')
+    res_bell = bell.findall(s)
+    res_stirling = stirling.findall(s)
+    for elem in res_bell:
+        val = str(eval(elem))
+        s = s.replace(elem, val, 1)
+    for elem in res_stirling:
+        val = str(eval(elem))
+        s = s.replace(elem, val, 1)
+    if len(s) > 75:
+        return None, ["Result contains more than 75 symbols", "Результат містить більше 75 знаків"][lg]
+    if s.find("ERR") != -1:
+        return None, ["n can`t be bigger than k", "n не може бути більшим за k"][lg]
+    if s.find("BIG") != -1:
+        return None, ["n can`t be bigger than 20", "n не може бути більшим за 20"][lg]
+    try:
+        res = str(eval(s))
+        return [s, res if s != res else ""], None
+    except:
+        return None, ["Incorrect expression", "Некорентий вираз"][lg]
+
+
