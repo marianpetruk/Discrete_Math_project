6840b2ea12cf477c466702acea377acccb39aca1
Roman Vey <roman.vey@gmail.com>
2017-01-30 23:16:40 +0200

    better output(fibo)

--- static/python/fibonacci.py
+++ static/python/fibonacci.py
@@ -4,7 +4,7 @@ def fibo(n, lg):
         return None, ["n not bigger than 1000", "n не більша за 1000"][lg]
     try:
         res = int(round((((1 + 5 ** 0.5) / 2) ** n - ((1 - 5 ** 0.5) / 2) ** n) / 5 ** 0.5, 1))
-        return res, None
+        return [res, n], None
     except:
         return None, ["Please, enter a number", "Будь ласка, введіть число"][lg]
 
