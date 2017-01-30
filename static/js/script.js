8744fc662cbaa59eb32f524d280b0c551feda717
Marian Petruk <marian.petruk@gmail.com>
2017-01-30 19:35:58 +0200

    some fixes/adds

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-    console.log("Start");
+	info = document.getElementById("info_table");
+	info.style.display = "block";
+	info.className = "animated zoomIn alert alert-dismissible alert-info";
     info_button = document.getElementById("info_button");
-    info = document.getElementById("info_table");
     info_button.onclick = function() {
         info.style.display = 'block';
         if (info.className == 'animated zoomOut alert alert-dismissible alert-info') {
