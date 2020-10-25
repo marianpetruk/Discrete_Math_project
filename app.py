f9bc31054dd018d6d07cfdce6df88d9d13b5381a
Bohdan Borkivskyi <bohdan.borkivskyi@quintagroup.org>
2020-10-25 21:01:14 +0200

    Simplify if statements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import os
 import sys
+from copy import deepcopy as dc
 sys.path.append(os.path.join(os.path.dirname(__file__), 'static', 'python'))
 import truth_table
 import relations
@@ -43,7 +44,7 @@ def truth():
         return render_template(path, result=None, errors=None, query="", lg=lg if lg else "en")
     data = request.form.get('formula')
     lg_val = 1 if lg == "uk" else 0
-    if data == "" or data is None:
+    if not data:
         return render_template(
             path,
             result=None,
@@ -51,12 +52,12 @@ def truth():
             query="",
             lg=lg if lg else "en"
         )
-    else:
-        try:
-            res, e = truth_table.start(data, lg_val)
-        except:
-            res, e = None, ["  Incorrect input", '  Некоректний ввід'][lg_val]
-        return render_template(path, result=res, errors=e, query=data.replace(" ", ""), lg=lg if lg else "en")
+
+    try:
+        res, e = truth_table.start(data, lg_val)
+    except:
+        res, e = None, ["  Incorrect input", '  Некоректний ввід'][lg_val]
+    return render_template(path, result=res, errors=e, query=data.replace(" ", ""), lg=lg if lg else "en")
 
 
 @app.route('/warshall', methods=['GET', 'POST'])
@@ -67,7 +68,7 @@ def warshall():
         return render_template(path, result=None, errors=None, query="", lg=lg if lg else "en")
     data = request.form.get('warshalla')
     lg_val = 1 if lg == "uk" else 0
-    if data == "" or data is None:
+    if not data:
         return render_template(
             path,
             result=None,
@@ -75,12 +76,12 @@ def warshall():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        try:
-            res, e = Warshalla.start(data, lg_val)
-        except:
-            res, e = None, ["  Incorrect input", '  Некоректний ввід'][lg_val]
-        return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
+
+    try:
+        res, e = Warshalla.start(data, lg_val)
+    except:
+        res, e = None, ["  Incorrect input", '  Некоректний ввід'][lg_val]
+    return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
 
 
 @app.route('/relations', methods=['GET', 'POST'])
@@ -91,7 +92,7 @@ def check_relation():
         return render_template(path, result=None, query="", errors=None, lg=lg if lg else "en")
     data = request.form.get('relation')
     lg_val = 1 if lg == "uk" else 0
-    if data == "" or data is None:
+    if not data:
         return render_template(
             path,
             result=None,
@@ -99,9 +100,9 @@ def check_relation():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        res, e = relations.main(data, lg_val)
-        return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
+
+    res, e = relations.main(data, lg_val)
+    return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
 
 
 @app.route('/fibonacci', methods=['GET', 'POST'])
@@ -112,7 +113,7 @@ def fibo():
         return render_template(path, result=None, query="", errors=None, lg=lg if lg else "en")
     data = request.form.get('fibo')
     lg_val = 1 if lg == "uk" else 0
-    if data == "" or data is None:
+    if not data:
         return render_template(
             path,
             result=None,
@@ -120,9 +121,9 @@ def fibo():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        res, e = fibonacci.fibo(data, lg_val)
-        return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
+
+    res, e = fibonacci.fibo(data, lg_val)
+    return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
 
 
 @app.route('/bell_stirling_numbers', methods=['GET', 'POST'])
@@ -141,9 +142,9 @@ def bs_numbers():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        res, e = Bell_Stirling_numbers.main(data, lg_val)
-        return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
+
+    res, e = Bell_Stirling_numbers.main(data, lg_val)
+    return render_template(path, result=res, query=data.replace(" ", ""), errors=e, lg=lg if lg else "en")
 
 
 @app.route('/composition', methods=['GET', 'POST'])
@@ -164,10 +165,35 @@ def composition():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        test_er = multimatrix.main(mat, '(1, 1)', lg_val)[1]
-        if test_er is not None:
-            e = test_er
+
+    test_er = multimatrix.main(mat, '(1, 1)', lg_val)[1]
+    if test_er is not None:
+        e = test_er
+        return render_template(
+            path,
+            result=None,
+            query1=mat.replace(" ", ""),
+            query2=pwr,
+            errors=e,
+            lg=lg if lg else "en"
+        )
+
+    mat = mat.replace(')(', '),(')
+    maxim = multimatrix.change_matrix(eval('[' + mat + ']'), [(1, 1)])[2]
+    res = dc(mat)
+    pwr = int(pwr)
+    e = None
+    for i in range(pwr - 1):
+        if res == '' and e is None:
+            return render_template(
+                path,
+                result=[[0]*maxim]*maxim,
+                query1=mat.replace(" ", ""),
+                query2=pwr,
+                errors=None,
+                lg=lg if lg else "en"
+            )
+        elif e is not None:
             return render_template(
                 path,
                 result=None,
@@ -177,50 +203,24 @@ def composition():
                 lg=lg if lg else "en"
             )
 
-        from copy import deepcopy as dc
-        mat = mat.replace(')(', '),(')
-        maxim = multimatrix.change_matrix(eval('[' + mat + ']'), [(1, 1)])[2]
-        res = dc(mat)
-        pwr = int(pwr)
-        e = None
-        for i in range(pwr - 1):
-            if res == '' and e is None:
-                return render_template(
-                    path,
-                    result=[[0]*maxim]*maxim,
-                    query1=mat.replace(" ", ""),
-                    query2=pwr,
-                    errors=None,
-                    lg=lg if lg else "en"
-                )
-            elif e is not None:
-                return render_template(
-                    path,
-                    result=None,
-                    query1=mat.replace(" ", ""),
-                    query2=pwr,
-                    errors=e,
-                    lg=lg if lg else "en"
-                )
-
-            res, e = multimatrix.main(res, mat, lg_val)
-            res = multimatrix.rechange_matrix(res)
-            tres = ''
-            for j in res:
-                tres += str(j)
-            res = dc(tres)
-        res = res.replace(')(', '),(')
-        res = eval('[' + res + ']')
-        res = multimatrix.change_matrix(res, [(1, 1)])[0]
-        res = multimatrix.add_to(res, maxim)
-        return render_template(
-            path,
-            result=res,
-            query1=mat.replace(" ", ""),
-            query2=pwr,
-            errors=e,
-            lg=lg if lg else "en"
-        )
+        res, e = multimatrix.main(res, mat, lg_val)
+        res = multimatrix.rechange_matrix(res)
+        tres = ''
+        for j in res:
+            tres += str(j)
+        res = dc(tres)
+    res = res.replace(')(', '),(')
+    res = eval('[' + res + ']')
+    res = multimatrix.change_matrix(res, [(1, 1)])[0]
+    res = multimatrix.add_to(res, maxim)
+    return render_template(
+        path,
+        result=res,
+        query1=mat.replace(" ", ""),
+        query2=pwr,
+        errors=e,
+        lg=lg if lg else "en"
+    )
 
 
 @app.route('/multiplication', methods=['GET', 'POST'])
@@ -233,7 +233,7 @@ def multiplication():
     data1 = request.form.get('matrix1')
     data2 = request.form.get('matrix2')
     lg_val = 1 if lg == "uk" else 0
-    if (data1 == "" or data1 is None) or (data2 == "" or data2 is None):
+    if not data1 or not data2:
         return render_template(
             path,
             result=None,
@@ -242,16 +242,16 @@ def multiplication():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        res, e = multimatrix.main(data1, data2, lg_val)
-        return render_template(
-            path,
-            result=res,
-            query1=data1.replace(" ", ""),
-            query2=data2.replace(" ", ""),
-            errors=e,
-            lg=lg if lg else "en"
-        )
+
+    res, e = multimatrix.main(data1, data2, lg_val)
+    return render_template(
+        path,
+        result=res,
+        query1=data1.replace(" ", ""),
+        query2=data2.replace(" ", ""),
+        errors=e,
+        lg=lg if lg else "en"
+    )
 
 
 @app.route('/combinatorics', methods=['GET', 'POST'])
@@ -274,7 +274,7 @@ def combinatorics():
     data_repeat = request.form.get('repeat')
     data_order = request.form.get('order')
     lg_val = 1 if lg == "uk" else 0
-    if data_m == "" or data_m is None or data_n == "" or data_n is None:
+    if not data_m or not data_n:
         return render_template(
             path,
             result=None,
@@ -285,19 +285,19 @@ def combinatorics():
             errors=["  Expression can`t be empty", "  Вираз не може бути порожнім"][lg_val],
             lg=lg if lg else "en"
         )
-    else:
-        answer, res, e = Combinatorics.main(data_m, data_n, data_order, data_repeat, lg_val)
-        data = [res, data_m, data_n, data_order, data_repeat, answer]
-        return render_template(
-            path,
-            result=data,
-            errors=e,
-            lg=lg if lg else "en",
-            query1=data_m.replace(" ", ""),
-            query2=data_n.replace(" ", ""),
-            query_repeat=data_repeat,
-            query_order=data_order
-        )
+
+    answer, res, e = Combinatorics.main(data_m, data_n, data_order, data_repeat, lg_val)
+    data = [res, data_m, data_n, data_order, data_repeat, answer]
+    return render_template(
+        path,
+        result=data,
+        errors=e,
+        lg=lg if lg else "en",
+        query1=data_m.replace(" ", ""),
+        query2=data_n.replace(" ", ""),
+        query_repeat=data_repeat,
+        query_order=data_order
+    )
 
 
 @app.errorhandler(500)
